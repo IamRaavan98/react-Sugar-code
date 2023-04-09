92f9a1459676c96306546c6187e4b11cbd9f2d74
Rohan Agrawal <Rohanagrawal1798@gmail.com>
2023-04-09 20:30:39 +0530

    removed unnessary files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Leftbar from "./components/leftbar/Leftbar";
-import Navbar from "./components/navbar/Navbar";
-import Rightbar from "./components/rightbar/Rightbar";
-import Update from "./components/update/Update";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-     
+     <div>
+      rohan  is my name
+     </div>
     </>
   );
 };
